b3919cfede4884d2dc2d7026c7050bb883af0396
Gabriel Lesemann <lesemanngabriel@gmail.com>
2025-02-14 13:21:15 -0600

    Add files via upload
    My first file!

---
+++ HELLO WORLD.py
@@ -0,0 +1,43 @@
+""" x=2
+if (x == 2):
+    print ("Hello world") """
+
+""" int1 = 0
+print(int1)
+float1 = 0.0
+print(float1)
+
+string1 = "Hello my name is Gabriel, I'm using double quotes because I don't want any apostrophe trouble."
+print(string1) """
+
+""" x=0
+while (x<5) and (x>=0):
+    x = x+1
+    print (x) """
+
+""" list1 = []
+list1.append(1)
+list1.append(2)
+list1.append(3)
+print(list1[0])
+print(list1[1])
+print(list1[2])
+
+for x in list1:
+    print(x) """
+
+# Start of Corey Schafer's videos
+""" message = 'Hello World'
+print(message)
+message = message.replace('World', 'Universe')
+print(message) """
+
+greeting = 'Hello'
+name = 'Gabriel'
+
+# message = greeting+', '+name+'. Welcome!'
+# message = '{}, {}. Welcome!'.format(greeting, name)
+# message = f'{greeting}, {name.upper()}. Welcome!'
+# print(message)
+
+print(help(str.lower))
